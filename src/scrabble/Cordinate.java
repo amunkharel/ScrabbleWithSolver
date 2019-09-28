7f7920c6b15bdad74914e99d4e84d317816941e1
Amun Kharel <akharel@cs.unm.edu>
2019-09-28 16:57:54 -0600

    Add More test code

---
+++ src/scrabble/Cordinate.java
@@ -0,0 +1,20 @@
+package scrabble;
+
+public class Cordinate {
+
+    private int x;
+    private int y;
+
+    public Cordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
--- src/scrabble/Main.java
+++ src/scrabble/Main.java
@@ -3,7 +3,13 @@ package scrabble;
 public class Main{
 
     public static void main(String[] args) {
-        Dictionary dictionary = new Dictionary("sowpods.txt");
-        dictionary.readAndStoreDictionaryInTrie();
+        //Dictionary dictionary = new Dictionary("sowpods.txt");
+        //dictionary.readAndStoreDictionaryInTrie();
+
+        Board board = new Board(15);
+
+        board.printBoard();
+
+
     }
 }
