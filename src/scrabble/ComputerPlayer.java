c9e573599a6097880ab2436bb994f326726cd9d3
Amun Kharel <akharel@cs.unm.edu>
2019-10-16 11:36:02 -0600

    Start adding javadoc documentation

--- src/scrabble/ComputerPlayer.java
+++ src/scrabble/ComputerPlayer.java
@@ -1074,15 +1074,31 @@ public class ComputerPlayer {
         score.resetCurrentCompScore();
     }
 
+    /**
+     * Checks if a given spot in the board is free for computer to play
+     *
+     * @param int x, row of board
+     * @param int y, column of board
+     * @return boolean, true if is free and vice versa
+     */
     public boolean isFree(int x, int y) {
         if(boardArray[x][y] == '-' || boardArray[x][y] == '1' ||
-                boardArray[x][y] == '2' || boardArray[x][y] == '3' || boardArray[x][y] == '4' ||
-        boardArray[x][y] == '@' || boardArray[x][y] == '!' || boardArray[x][y] == '$') {
+                boardArray[x][y] == '2' || boardArray[x][y] == '3'
+                || boardArray[x][y] == '4' ||
+        boardArray[x][y] == '@' || boardArray[x][y] == '!'
+                || boardArray[x][y] == '$') {
             return true;
         }
         return  false;
     }
 
+    /**
+     * Checks if the given spot of the board is out of index
+     * and returns true or false
+     * @param int i, row of board
+     * @param int j, column of board
+     * @return, true if is out of index and vice versa
+     */
     public boolean boardOutOfIndex(int i, int j) {
         if(i < 0 || j <0 || i > board.getSize() - 1 || j > board.getSize() - 1){
             return true;
@@ -1090,6 +1106,12 @@ public class ComputerPlayer {
         return  false;
     }
 
+    /**
+     * Updates the tray of the computer after the move is made
+     *
+     * Takes in the word and removes it from the tray
+     * @param String word, Word played by the computer
+     */
     public void updateTray(String word) {
         int replaceNeeded = 0;
         List<Integer> trayIndex = new ArrayList<Integer>();
@@ -1124,6 +1146,13 @@ public class ComputerPlayer {
 
     }
 
+    /**
+     * Checks if a character played by the computer is
+     * capital. If capital returns true
+     *
+     * @param char letter, letter being checked
+     * @return boolean, true if a letter is capital
+     */
     public boolean letterIsCapital(char character) {
         int asci = character - 'A';
 
@@ -1135,6 +1164,15 @@ public class ComputerPlayer {
         }
     }
 
+    /**
+     * Removes character at a given index
+     * from a string and returns the string
+     *
+     * @param String str, String from which character
+     *               is being deleted
+     * @param int p, index of the character
+     * @return String with the character removed
+     */
     public  String charRemoveAt(String str, int p) {
         return str.substring(0, p) + str.substring(p + 1);
     }
