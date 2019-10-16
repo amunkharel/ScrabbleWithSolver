6e43c4514f5a53d4d3f28ef7b7e60ce50b626d8e
Amun Kharel <akharel@cs.unm.edu>
2019-10-16 12:49:30 -0600

    Add more javadoc

--- src/scrabble/Dictionary.java
+++ src/scrabble/Dictionary.java
@@ -1,19 +1,41 @@
+/**
+ * Project 3 - CS351, Fall 2019, Dictionary to store words from the file
+ * @version Date 2019-10-15
+ * @author Amun Kharel
+ *
+ *
+ */
+
 package scrabble;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-//Source -- https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
 public class Dictionary{
+
+    /** Filename of the dictionary */
     private String filename;
+
+    /** Trie objects to store the words */
     private Trie root = new Trie();
+
+    /** Number of letters in English words*/
     private int size = 26;
 
+    /**
+     * Constructor for Dictionary
+     *
+     * @param String fileName, name of the file
+     */
     public Dictionary(String filename) {
         this.filename = filename;
     }
 
+    /**
+     * Reads words from the file and stores it in
+     * the trie data structure
+     */
     public void readAndStoreDictionaryInTrie() {
         int counter = 0;
         String pathname = "resources/" + filename;
@@ -26,8 +48,6 @@ public class Dictionary{
                     root.insertWord(root, str);
                     counter++;
                 }
-
-                //System.out.println(str);
             }
 
             sc.close();
@@ -38,6 +58,12 @@ public class Dictionary{
 
     }
 
+    /**
+     * Checks if a word is valid or not
+     *
+     * @param String word, word passed to be checked
+     * @return boolean, checks if a move is valid or not
+     */
     public boolean isValidMove(String word ) {
         if(root.isValidWord(word, root)) {
             return true;
@@ -45,12 +71,28 @@ public class Dictionary{
         return  false;
     }
 
+    /**
+     * Returns all the valid words if a word has
+     * one wildcard
+     *
+     * @param String word, word with one wildcard
+     * @return boolean[] , returns boolean where word is valid
+     * if boolean [0] is true, 'a' is a valid word instead of wildcard
+     */
     public boolean[] validWordsForOneWildCard(String word) {
         boolean [] validwords  = new boolean[size];
         validwords = root.validWordsForOneWildCard(word, root);
         return validwords;
     }
 
+    /**
+     * Returns all the valid words if a word has
+     * two wildcard
+     *
+     * @param String word, word with two wildcard
+     * @return boolean[][] , returns boolean where word is valid
+     * if boolean [0][1] is true, 'a' and 'b' is a valid word instead of wildcards
+     */
     public boolean [][] validWordsForTwoWildCard(String word) {
         boolean [][] validwords = new boolean[size][size];
         validwords = root.validWordsForTwoWildCard(word, root);
@@ -58,4 +100,3 @@ public class Dictionary{
     }
 
 }
-
